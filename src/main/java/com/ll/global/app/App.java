2323da0b270043c26ab07a14e5e9201e5cf5cea1
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-01-30 14:18:57 +0900

    성공 테스트 작성

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -1,4 +1,17 @@
 package com.ll.global.app;
 
+import java.util.Scanner;
+
 public class App {
+
+    private final Scanner scanner;
+    public App(final Scanner scanner) {
+
+        this.scanner=scanner;
+    }
+
+    public void run() {
+
+        System.out.println("==명언앱==");
+    }
 }
--- src/test/java/com/ll/global/AppTest.java
+++ src/test/java/com/ll/global/AppTest.java
@@ -6,22 +6,47 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AppTest {
     @Test
-    @DisplayName("테스트 명언앱")
+    @DisplayName("프로그램 시작 시 ==명언앱==  출력")
     void t1() {
+
+        Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+
+
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
-        new App();
+        new App(scanner).run();
 
         String out = byteArrayOutputStream.toString().trim();
 
         TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
 
         assertThat(out).contains("==명언앱==");
+    }
+
+    @Test
+    @DisplayName("종료")
+    void t2() {
+
+        Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App(scanner).run();
+
+        String out = byteArrayOutputStream.toString().trim();
+
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
     }
 
 }
