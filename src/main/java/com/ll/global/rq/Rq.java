8eb841f2fba6f3938945fdde52249451c848e515
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-01-30 19:21:50 +0900

    Rq 도입 t4 삭제 성공테스트 구현

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,6 +1,7 @@
 package com.ll.global.rq;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,19 +11,31 @@ public class Rq {
     private final String queryString;
     private Map<String , String> params;
     public Rq(String cmd) {
+
         final String[] cmdBits = cmd.split("\\?" , 2);
+
        action = cmdBits[0].trim();
+
        queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
+
        params = new HashMap<>();
 
        if (queryString.isBlank()) return ;
 
-       final String [] paramBits = queryString.split("=" , 2); //삭제?id=2
+        Arrays
+                .stream(queryString
+                .split("&"))
+                .forEach(param -> {
 
-       final String paramName = paramBits[0].trim();
-       final String paramValue = paramBits[1].trim();
+                    final String [] paramBits = param.split("=" , 2); //삭제?id=2
 
-       params.put(paramName , paramValue);
+                    final String paramName = paramBits[0].trim();
+
+                    final String paramValue = paramBits[1].trim();
+
+                    params.put(paramName , paramValue);
+
+                });
     }
 
     public String getAction() {
