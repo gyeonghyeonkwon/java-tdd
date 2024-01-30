121ca0a44845e434e6602d1d0e1a46f9674515ea
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-01-30 19:30:32 +0900

    Rq 도입 t4 삭제 리팩토링테스트 구현

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -2,40 +2,25 @@ package com.ll.global.rq;
 
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Rq {
-
     private final String action;
     private final String queryString;
-    private Map<String , String> params;
-    public Rq(String cmd) {
+    private Map<String, String> params;
 
-        final String[] cmdBits = cmd.split("\\?" , 2);
-
-       action = cmdBits[0].trim();
-
-       queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
-
-       params = new HashMap<>();
-
-       if (queryString.isBlank()) return ;
-
-        Arrays
-                .stream(queryString
-                .split("&"))
-                .forEach(param -> {
-
-                    final String [] paramBits = param.split("=" , 2); //삭제?id=2
-
-                    final String paramName = paramBits[0].trim();
-
-                    final String paramValue = paramBits[1].trim();
-
-                    params.put(paramName , paramValue);
-
-                });
+    public Rq(final String cmd) {
+        final String[] cmdBits = cmd.split("\\?", 2);
+        action = cmdBits[0].trim();
+        queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
+        params = Arrays.stream(queryString.split("&"))
+                .filter(param -> param.contains("="))
+                .map(param -> param.split("=", 2))
+                .collect(Collectors.toMap(
+                        paramBits -> paramBits[0].trim(),
+                        paramBits -> paramBits[1].trim(),
+                        (existing, replacement) -> replacement));
     }
 
     public String getAction() {
@@ -43,9 +28,6 @@ public class Rq {
     }
 
     public String getParameter(final String paramName) {
-
         return params.get(paramName);
-
-
     }
 }
